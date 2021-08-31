095cc28c74a6d966b96553d1140c3f656b34c3e8
Sandaruwan-Lakshitha <sandaru.laka@gmail.com>
2021-08-31 16:18:19 +0530

    add styled-componets

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import styled from "../node_modules/styled-components/";
+
+const Title = styled.h1`
+  font-size: 3em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Wrapper>
+        <Title>It's gonna be awesome</Title>
+      </Wrapper>
     </div>
   );
 }
