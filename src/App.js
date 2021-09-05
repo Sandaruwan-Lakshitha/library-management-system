92f796e1d42fd41c3d605676951af3ffd6480bb4
Sandaruwan-Lakshitha <sandaru.laka@gmail.com>
2021-09-06 01:23:17 +0530

    add layout for app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import styled from "../node_modules/styled-components/";
-
-const Title = styled.h1`
-  font-size: 3em;
-  text-align: center;
-  color: palevioletred;
-`;
-
-const Wrapper = styled.section`
-  padding: 4em;
-  background: papayawhip;
-`;
+import { Header, Main, Footer } from "./componets/layout";
 
 function App() {
   return (
-    <div>
-      <Wrapper>
-        <Title>It's gonna be awesome</Title>
-      </Wrapper>
-    </div>
+    <>
+      <Header>This is the header</Header>
+      <Main>this is the main compopnets</Main>
+      <Footer>This is the footer</Footer>
+    </>
   );
 }
 
