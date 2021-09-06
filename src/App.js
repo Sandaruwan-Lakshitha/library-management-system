ade6e3cfae4504f622aa21897342c9c7a75687ba
Sandaruwan-Lakshitha <sandaru.laka@gmail.com>
2021-09-06 10:23:29 +0530

    add Navbar compoent

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import { Header, Main, Footer } from "./componets/layout";
+import { NavBar, NavItem, NavLink } from "./componets/Navbar";
 
 function App() {
   return (
     <>
-      <Header>This is the header</Header>
+      <Header>
+        <NavBar>
+          <NavItem href="#">
+            <NavLink>Catalog</NavLink>
+          </NavItem>
+          <NavItem href="#">
+            <NavLink>Dashboard</NavLink>
+          </NavItem>
+
+        </NavBar>
+      </Header>
       <Main>this is the main compopnets</Main>
       <Footer>This is the footer</Footer>
     </>
