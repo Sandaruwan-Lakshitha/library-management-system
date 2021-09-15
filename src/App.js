d74c539c28eda90a8d969771a9e1953b3940280a
Sandaruwan-Lakshitha <sandaru.laka@gmail.com>
2021-09-15 16:51:39 +0530

    create http requests

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ function App() {
    let routes = (
       <Suspense fallback={<Spinner />}>
          <Switch>
+            <Route exact path="/" component={Dashboard} />
             <Route exact path={DASHBOARD} component={Dashboard} />
             <Route exact path={CATALOG} component={Spinner} />
             <Route component={NotFound} />
