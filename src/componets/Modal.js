36fb14b452c6a90ad3ec56e06d3fe36ec1e5f13e
Sandaruwan-Lakshitha <sandaru.laka@gmail.com>
2021-09-29 13:45:27 +0530

    Styling tweaks

--- src/componets/Modal.js
+++ src/componets/Modal.js
@@ -18,7 +18,7 @@ export const DialogBox = styled.div`
    height: auto;
    top: 50%auto;
    left: 50%;
-   transform: 50%;
+   transform: translate(-50%, -50%);
    border-radius: 10px;
    padding: 0.75rem;
    color: rgba(0, 139, 0, 0.7);
