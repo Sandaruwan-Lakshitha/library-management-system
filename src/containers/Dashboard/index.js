1028cd3d3e5a04cd41a0a250a4cb1748ad292822
Sandaruwan-Lakshitha <sandaru.laka@gmail.com>
2021-10-11 02:32:05 +0530

    Use redux store in Dashboard

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import Tabs from "../../componets/Tabs";
 import Spinner from "../../componets/Spinner";
 
 import Books from "../Dashboard/Books/index";
 
+import {setBooks} from "../../store/booksSlice";
 import { getBooks } from "../../api/bookAPI";
 
 export const Dashboard = () => {
    const [isLoading, setIsLoading] = useState(false);
-   const [book, setBook] = useState([]);
+   //const [book, setBook] = useState([]);
+
+   const booksFromRedux = useSelector((state) => state.books.value);
+   const dispatch = useDispatch();
 
    useEffect(() => {
       setIsLoading(true);
       getBooks()
          .then((response) => {
             if (!response.error) {
-               setBook(response.data);
+               console.log(response.data);
+               dispatch(setBooks(response.data));
             }
          })
          .catch((error) => {
@@ -25,18 +31,14 @@ export const Dashboard = () => {
          .finally(() => {
             setIsLoading(false);
          });
-   }, []);
+   }, [dispatch]);
 
    const contents = [
-      { title: "Books", elements: <Books catalog={book} /> },
+      { title: "Books", elements: <Books catalog={booksFromRedux} /> },
       { title: "Members", elements: <h1>Conetct of members go here</h1> },
    ];
 
-   return isLoading ? (
-      <Spinner />
-   ) : (
-     <Tabs contents={contents} />
-   );
+   return isLoading ? <Spinner /> : <Tabs contents={contents} />;
 };
 
 export default Dashboard;
