24b272877665129a7a3fac4b38385ff6ccef35df
Sandaruwan-Lakshitha <sandaru.laka@gmail.com>
2021-10-11 14:36:14 +0530

    Add and delete redux functionalities

--- src/store/booksSlice.js
+++ src/store/booksSlice.js
@@ -9,18 +9,28 @@ export const bookSlice = createSlice({
       setBooks: (state, action) => {
          state.value = action.payload;
       },
-      updateBook: (state,action) => {
+      updateBook: (state, action) => {
          const updatedBooks = [...state.value];
          const id = action.payload.id;
-         const index = updatedBooks.findIndex(
-            (element) => element.id === id
-         );
-         updatedBooks.splice(index,1,action.payload);
+         const index = updatedBooks.findIndex((element) => element.id === id);
+         updatedBooks.splice(index, 1, action.payload);
+         state.value = updatedBooks;
+      },
+      addBook: (state, action) => {
+         const updatedBooks = [...state.value];
+         updatedBooks.push(action.payload);
+         state.value = updatedBooks;
+      },
+      deleteBook: (state, action) => {
+         const updatedBooks = [...state.value];
+         const id = action.payload.id;
+         const index = updatedBooks.findIndex((element) => element.id === id);
+         updatedBooks.splice(index, 1);
          state.value = updatedBooks;
       },
    },
 });
 
-export const { setBooks, updateBook } = bookSlice.actions;
+export const { setBooks, updateBook, addBook, deleteBook } = bookSlice.actions;
 
 export default bookSlice.reducer;
